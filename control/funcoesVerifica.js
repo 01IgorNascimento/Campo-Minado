ac894a8951e51b4a9a99d2e8afc31e3b5b5c4782
Igor Nascimento <igor.silva@sptech.school>
2023-02-03 10:26:22 -0300

    Projeto Finalizado

---
+++ control/funcoesVerifica.js
@@ -0,0 +1,118 @@
+var bombasPertoArray=[]
+var camposProibidos=[]
+var camposBeiradaDireita=[8,17,26,35,44,53,62]
+var camposBeiradaEsquerda=[0,9,18,27,36,45,54]
+  
+ function verificaImpar(){
+
+    if(divCampo[x].id % 2 == 0){
+        divCampo[x]._impar=false
+        divCampo[x].elementoHtml.style.backgroundColor = "#01455a";
+    }
+    if(divCampo[x].id % 2 == 1){
+        divCampo[x]._impar=true
+        divCampo[x].elementoHtml.style.backgroundColor = "#5f98ab";
+    }
+    }
+
+function verificaBeirada(valore,arrayPassado){
+var bombasPertoArray=[]
+    bombasPertoArray[0]=valore-9
+    bombasPertoArray[1]=valore+9
+    bombasPertoArray[2]= valore+1
+    bombasPertoArray[3]= valore-1
+    bombasPertoArray[4]= valore-10
+    bombasPertoArray[5]= valore+10
+    bombasPertoArray[6]= valore+8
+    bombasPertoArray[7]= valore-8
+
+    var x=0
+
+    while(x<7){
+        if(divCampo[valore].id==camposBeiradaDireita[x]){
+            bombasPertoArray.splice(7,1)
+            bombasPertoArray.splice(5,1)
+            bombasPertoArray.splice(2,1)
+
+            divCampo[valore].beirada=true
+        }
+        else{
+
+        }
+        x++
+    }
+x=0
+    while(x<7){
+        
+        if(divCampo[valore].id==camposBeiradaEsquerda[x]){
+
+            //[0]=-9    [4]=-10        [3]=-1       [6]=+8
+            bombasPertoArray.splice(6,1)
+            bombasPertoArray.splice(4,1)
+            bombasPertoArray.splice(3,1)
+      
+            divCampo[valore].beirada=true
+        }
+        else{
+
+        }
+        x++
+    }
+if(arrayPassado==''){
+    return bombasPertoArray
+
+}else{
+
+    alert(arrayPassado)
+}
+} 
+    function verificaBomba(valor){
+    
+        if(valor._bomba==false){
+              valor.elementoHtml.style.backgroundColor = "#daccb1";
+              if(valor._impar==true){
+                 valor.elementoHtml.style.backgroundColor = "#aca18d";
+             }
+     
+         }
+         else{
+            perdeu()
+             
+         }
+     } 
+    
+     
+
+     
+function avisaBombasPerto(valor){
+    var x=0
+    var y=0
+
+        while(y<8 && x<63){
+            
+            if(valor[y]<63 && valor[y]>=0){
+                if(divCampo[x].id==valor[y]){
+                    if(divCampo[x].beirada!=true){
+                        
+                        divCampo[x]._bombasProximas+=1
+                        copia_P[x].textContent=divCampo[x].bombasProximas
+                        copia_P[x].classList.add("bombasProximas2")
+                        alteraTextoPrimeirosCliques()
+                        x=0
+                        y++
+                    }
+                    else{
+
+                    }    
+                }
+            }
+            else{
+                y++
+            }
+        x++
+        if(y < 8 && x==63){
+            x=0
+                
+            }
+        }
+    }
